1fe68a9e4b7f37fafb4e2e9a7d0acda43952bbde
HishamStark <hisham222111@gmail.com>
2024-10-23 05:26:49 +0530

    new pdf

--- pg6.c
+++ pg6.c
@@ -1,47 +1,47 @@
-#include<stdio.h>
-#define MAX 20
-void read_matrix(int a[10][10],int row, int column);
-void print_sparse(int b[MAX][3]);
-void create_sparse(int a[10][10], int row, int column, int b[MAX][3]);
-int main()
-{
-	int a[10][10],b[MAX][3],row,column;
-	printf("\nEnter the size of matrix (rows,columns): ");
-	scanf("%d%d",&row,&column);
-	read_matrix(a,row,column);
-	create_matrix(a, row, column, b);
-	print_sparse(b);
-	return 0;	
-}
-void read_matrix(int a[10][10],int row, int column)
-{
-	int i,j;
-	printf("\nEnter elements of matrix\n");
-	for(i=0;i<row;i++)
-	{
-		for(j=0;j<column;j++)
-		{
-			printf("[%d][%d]: ",i,j);
-			scanf("%d", &a[i][j]);
-		}
-	}
-}
-void create_sparse(int a[10][10],int row,int column,int b[MAX][3])
-{
-	int i,j,k;
-	k=1;
-	b[0][0]=row;
-	b[0][1]=column;
-	for(i=0;i<row;i++)
-	{
-		for(j=0;j<column;j++)
-		{
-			if(a[i][j]!=0)
-			{
-				b[k][0]=i;
-				b[k][1]=j;
-				b[k][2]=a[i][j];
-			}
-		}
-	}
+#include <stdio.h>
+
+#define MAX 100
+
+struct SparseMatrix {
+    int row;
+    int col;
+    int value;
+};
+
+int main() {
+    int matrix[10][10];
+    struct SparseMatrix sparse[MAX];
+    int i, j, rows, cols, nonZeroCount = 0;
+
+    printf("Enter the number of rows and columns of the matrix:\n");
+    scanf("%d%d", &rows, &cols);
+
+    printf("Enter the elements of the matrix:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+            if (matrix[i][j] != 0) {
+                sparse[nonZeroCount].row = i;
+                sparse[nonZeroCount].col = j;
+                sparse[nonZeroCount].value = matrix[i][j];
+                nonZeroCount++;
+            }
+        }
+    }
+
+    printf("\nOriginal Matrix:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\nSparse Matrix Representation (Triplet Format):\n");
+    printf("Row Column Value\n");
+    for (i = 0; i < nonZeroCount; i++) {
+        printf("%d %d %d\n", sparse[i].row, sparse[i].col, sparse[i].value);
+    }
+
+    return 0;
 }
